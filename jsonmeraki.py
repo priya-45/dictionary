26204e81f38eff5be5cfccea6de81b661142391f
priya-45 <98081072+priya-45@users.noreply.github.com>
2022-01-21 13:22:12 +0530

    Add files via upload

---
+++ jsonmeraki.py
@@ -0,0 +1,64 @@
+import json
+
+dict1 ={
+    "emp1": {
+        "name": "Lisa",
+        "designation": "programmer",
+        "age": "34",
+        "salary": "54000"
+    },
+    "emp2":{
+        "name": "Elis",
+        "designation": "Trainee",
+        "age": "24",
+        "salary": "40000"
+    }}
+print(dict1)
+
+
+# a="my name is priya"
+# a.split()
+# print(a)
+# m={}
+# i=0
+# c1=0
+# c=" "
+# while i<len(a):
+#     t=(a[i])
+#     if a[i]==" ":
+#         c1=c1+1
+#     if c1==1:
+#         m.up
+#     i=i+1
+#     j=j+1
+
+
+
+# a="my name is priya"a="my name is priya"
+# 2	i=0
+# 3	b=a.split()
+# 4	print(b)
+# 5	m={}
+# 6	while i<len(b):
+# 7	    t=(b[i])
+# 8	    c=i
+# 9	    m.update({t:i})
+# 10	    if i==2:
+# 11	        pass
+# 12	    i=i+1
+# 13	    m.update({"space1":i})
+# 14	print(m)
+
+# 2	i=0
+# 3	b=a.split()
+# 4	print(b)
+# 5	m={}
+# 6	while i<len(b):
+# 7	    t=(b[i])
+# 8	    c=i
+# 9	    m.update({t:i})
+# 10	    if i==2:
+# 11	        pass
+# 12	    i=i+1
+# 13	    m.update({"space1":i})
+# 14	print(m)
